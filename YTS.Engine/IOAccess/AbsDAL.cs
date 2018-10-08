b8897ec5f660eeca3d451e8a62fa3bb983568960
YellowTulipShow <1426689530@qq.com>
2018-10-08 17:46:12 +0800

    add AbsDAL_OnlyQuery.cs

--- YTS.Engine/IOAccess/AbsDAL.cs
+++ YTS.Engine/IOAccess/AbsDAL.cs
@@ -5,18 +5,12 @@ using YTS.Tools.Model;
 
 namespace YTS.Engine.IOAccess
 {
-    public abstract class AbsDAL<M, W, P, PI> : IDAL<M, W, P, PI>
+    public abstract class AbsDAL<M, W, P, PI> : AbsDAL_OnlyQuery<M, W, P, PI>
         where M : AbsShineUpon
         where P : ShineUponParser<M, PI>
         where PI : ShineUponInfo
     {
-        public M DefaultModel = null;
-        public P Parser = null;
-
-        public AbsDAL() {
-            this.DefaultModel = ReflexHelp.CreateNewObject<M>();
-            this.Parser = ReflexHelp.CreateNewObject<P>();
-        }
+        public AbsDAL() : base() { }
 
         public abstract bool Insert(M model);
 
@@ -25,16 +19,5 @@ namespace YTS.Engine.IOAccess
         public abstract bool Delete(W where);
 
         public abstract bool Update(KeyObject[] kos, W where);
-
-        public abstract M[] Select(int top, W where, KeyBoolean[] sorts = null);
-
-        public abstract M[] Select(int pageCount, int pageIndex, out int recordCount, W where, KeyBoolean[] sorts);
-
-        public abstract int GetRecordCount(W where);
-
-        public virtual M GetModel(W where, KeyBoolean[] sorts) {
-            M[] list = Select(1, where, null);
-            return (CheckData.IsSizeEmpty(list)) ? null : list[0];
-        }
     }
 }
---
+++ YTS.Engine/IOAccess/AbsDAL_OnlyQuery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using YTS.Engine.ShineUpon;
+using YTS.Tools;
+using YTS.Tools.Model;
+
+namespace YTS.Engine.IOAccess
+{
+    public abstract class AbsDAL_OnlyQuery<M, W, P, PI> : IDAL_OnlyQuery<M, W, P, PI>
+        where M : AbsShineUpon
+        where P : ShineUponParser<M, PI>
+        where PI : ShineUponInfo
+    {
+        public M DefaultModel = null;
+        public P Parser = null;
+
+        public AbsDAL_OnlyQuery() {
+            this.DefaultModel = ReflexHelp.CreateNewObject<M>();
+            this.Parser = ReflexHelp.CreateNewObject<P>();
+        }
+
+        public abstract M[] Select(int top, W where, KeyBoolean[] sorts);
+
+        public abstract M[] Select(int pageCount, int pageIndex, out int recordCount, W where, KeyBoolean[] sorts);
+
+        public abstract int GetRecordCount(W where);
+
+        public virtual M GetModel(W where, KeyBoolean[] sorts) {
+            M[] list = Select(1, where, null);
+            return (CheckData.IsSizeEmpty(list)) ? null : list[0];
+        }
+    }
+}
