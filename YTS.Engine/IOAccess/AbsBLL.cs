de7a5df8940907f7fd8be3892821442121c0e612
YellowTulipShow <1426689530@qq.com>
2018-10-09 09:45:44 +0800

    add note

--- YTS.Engine/IOAccess/AbsBLL.cs
+++ YTS.Engine/IOAccess/AbsBLL.cs
@@ -5,6 +5,14 @@ using YTS.Tools.Model;
 
 namespace YTS.Engine.IOAccess
 {
+    /// <summary>
+    /// 抽象-业务逻辑层(Business Logic Layer)
+    /// </summary>
+    /// <typeparam name="M">数据映射模型</typeparam>
+    /// <typeparam name="D">抽象-数据访问层(Data Access Layer)</typeparam>
+    /// <typeparam name="W">查询条件</typeparam>
+    /// <typeparam name="P">解析器</typeparam>
+    /// <typeparam name="PI">解析信息数据模型</typeparam>
     public abstract class AbsBLL<M, D, W, P, PI> : AbsBLL_OnlyQuery<M, D, W, P, PI>
         where M : AbsShineUpon
         where D : AbsDAL<M, W, P, PI>
--- YTS.Engine/IOAccess/AbsDAL.cs
+++ YTS.Engine/IOAccess/AbsDAL.cs
@@ -5,7 +5,14 @@ using YTS.Tools.Model;
 
 namespace YTS.Engine.IOAccess
 {
-    public abstract class AbsDAL<M, W, P, PI> : AbsDAL_OnlyQuery<M, W, P, PI>
+    /// <summary>
+    /// 抽象-数据访问层(Data Access Layer)
+    /// </summary>
+    /// <typeparam name="M">数据映射模型</typeparam>
+    /// <typeparam name="W">查询条件</typeparam>
+    /// <typeparam name="P">解析器</typeparam>
+    /// <typeparam name="PI">解析信息数据模型</typeparam>
+    public abstract class AbsDAL<M, W, P, PI> : AbsDAL_OnlyQuery<M, W, P, PI>, IDAL<M, W, P, PI>
         where M : AbsShineUpon
         where P : ShineUponParser<M, PI>
         where PI : ShineUponInfo
