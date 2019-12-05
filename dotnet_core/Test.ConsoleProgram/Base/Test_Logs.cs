07a287df8b7b8e38d00ba38a8ec0791978749627
YellowTulipShow <1426689530@qq.com>
2019-12-05 17:28:54 +0800

    add set Test_Logs.cs file

---
+++ dotnet_core/Test.ConsoleProgram/Base/Test_Logs.cs
@@ -0,0 +1,18 @@
+using System;
+using YTSCharp.Tools;
+
+namespace Test.ConsoleProgram.Base
+{
+    public class Test_Logs : CaseModel
+    {
+        public Test_Logs()
+        {
+            this.NameSign = "测试日志";
+            this.ExeEvent = () =>
+            {
+                Console.WriteLine("输出普通日志: Hello Logs Recore!");
+                return true;
+            };
+        }
+    }
+}
