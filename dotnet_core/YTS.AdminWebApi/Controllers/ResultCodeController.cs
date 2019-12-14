47b6b3499e6cdbe7095b8f472606c165e8c01bad
YellowTulipShow <1426689530@qq.com>
2019-12-14 11:54:55 +0800

    add ResultCode foreach show

---
+++ dotnet_core/YTS.AdminWebApi/Controllers/ResultCodeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using YTS.Tools;
+using YTS.Data.Models;
+using YTS.Data.Models.WebApi;
+
+namespace YTS.AdminWebApi.Controllers
+{
+    public class ResultCodeController : BaseApiController
+    {
+        /// <summary>
+        /// 获取所有请求代码的详情
+        /// </summary>
+        [HttpGet]
+        public object GetResultCodes()
+        {
+            EnumInfo[] infos = EnumInfo.AnalysisList<ResultCode>();
+            var list = infos
+                .Select(info => new
+                {
+                    code = info.IntValue,
+                    name = info.Name,
+                    remark = info.Explain,
+                })
+                .ToList();
+            return list;
+        }
+    }
+}
