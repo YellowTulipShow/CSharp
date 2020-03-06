a260867292e4e24f7a8a3c239b76896b10851e81
YellowTulipShow <main@yellowtulipshow.site>
2020-03-06 10:21:42 +0800

    JsonConfigHelper.cs

---
+++ dotnet_core/Test.ConsoleProgram/TestTools/TestJsonConfigHelper.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using YTS.Tools;
+
+namespace Test.ConsoleProgram.TestTools
+{
+    public class JsonConfigHelper : CaseModel
+    {
+        public JsonConfigHelper()
+        {
+            this.NameSign = "测试JSON配置文件帮助类";
+            this.SonCases = new List<CaseModel>()
+            {
+            };
+        }
+    }
+}
