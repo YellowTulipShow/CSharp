95a42b1eaccbd47d4c91898a17576cf8ce06c0e4
YellowTulipShow <main@yellowtulipshow.site>
2020-03-24 23:24:42 +0800

    设置SwaggerAuthTokenHeaderParameter

---
+++ dotnet_core/YTS.AdminWebApi/_Code/SwaggerAuthTokenHeaderParameter.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Authorization;
+
+namespace YTS.WebApi
+{
+    public class SwaggerAuthTokenHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+            var attrs = context.ApiDescription.ActionDescriptor.AttributeRouteInfo;
+            var descriptor = context.ApiDescription.ActionDescriptor as ControllerActionDescriptor;
+            if (descriptor == null)
+                return;
+            var customAttrs = descriptor.MethodInfo.GetCustomAttributes(inherit: true);
+            var typeAllowAnonymous =  typeof(AllowAnonymousAttribute);
+            bool isHaveAllowAnonymous = customAttrs.Any(a => a.GetType().Equals(typeAllowAnonymous));
+            if (isHaveAllowAnonymous)
+                return;
+            operation.Parameters.Add(new OpenApiParameter()
+            {
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Description = "Bearer XXXXXXXXX",
+                Required = true //是否必选
+            });
+        }
+    }
+}
