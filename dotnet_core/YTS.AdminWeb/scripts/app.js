d32e4da1f19c41d7db516324de0e9b45ed1c5b70
YellowTulipShow <main@yellowtulipshow.site>
2020-03-30 23:12:52 +0800

    样式修改

--- dotnet_core/YTS.AdminWeb/scripts/app.js
+++ dotnet_core/YTS.AdminWeb/scripts/app.js
@@ -1,28 +1,6 @@
-// (function() {
-//     // 检测IE
-//     if ('undefined' == typeof (document.body.style.maxHeight)) {
-//         window.location.href = '/ie6update.html';
-//     }
-// })();
-
 (function() {
-    window.Api = function(url) {
-        url = url.trim('/');
-        domain = 'https://localhost:6121';
-        return domain + '/' + url;
-    }
-})();
-
-(function() {
-    //jquery全局配置
     $.ajaxSetup({
-        // dataType: "json",
-        // cache: false,
-        // headers: {
-        //     "token": token
-        // },
         beforeSend: function (xhr) {
-            // 可以设置自定义标头
             var token = window.localStorage.getItem('jwtToken');
             if (token) {
                 xhr.setRequestHeader('Authorization', 'Bearer ' + token);
@@ -36,4 +14,15 @@
             }
         }
     });
+})();
+
+(function() {
+    var domain = 'https://localhost:6121';
+    window.Api = {
+        BaseUrl: domain,
+        Call: function(url) {
+            url = (url || '').trim('/');
+            return domain + '/' + url;
+        },
+    }
 })();
