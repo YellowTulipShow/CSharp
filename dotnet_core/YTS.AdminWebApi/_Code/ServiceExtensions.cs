ff333ad3e4de5d237b8e57ee5660b845637f001e
YellowTulipShow <main@yellowtulipshow.site>
2020-04-01 09:21:11 +0800

    文字修改

--- dotnet_core/YTS.AdminWebApi/_Code/ServiceExtensions.cs
+++ dotnet_core/YTS.AdminWebApi/_Code/ServiceExtensions.cs
@@ -124,7 +124,7 @@ namespace YTS.WebApi
                 // 必须是 oauth2
                 c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
                 {
-                    Description = "JWT授权(数据将在请求头中进行传输) 直接在下框中输入Bearer {token}（注意两者之间是一个空格）\"",
+                    Description = "JWT授权 (数据将在请求头中进行传输) 直接在下框中输入 Bearer {token} (注意两者之间是一个空格)",
                     Name = "Authorization", // jwt默认的参数名称
                     In = ParameterLocation.Header, // jwt默认存放Authorization信息的位置(请求头中)
                     Type = SecuritySchemeType.ApiKey
