d7d03ea0dc9a2865a48282e5cf47d3f7c241eac4
YellowTulipShow <main@yellowtulipshow.site>
2020-04-06 17:28:46 +0800

    方法设置

--- dotnet_core/YTS.AdminWebApi/Controllers/SystemManage/MenusController.cs
+++ dotnet_core/YTS.AdminWebApi/Controllers/SystemManage/MenusController.cs
@@ -288,5 +288,55 @@ namespace YTS.AdminWebApi.Controllers
                 return model;
             }
         }
+
+        [HttpPost]
+        public Result<object> EditMenus(Menus model)
+        {
+            var result = new Result<object>();
+            if (model == null)
+            {
+                result.Code = ResultCode.BadRequest;
+                result.Message = "模型为空!";
+                return result;
+            }
+            var ID = model.ID;
+            if (ID <= 0)
+            {
+                model.AddTime = DateTime.Now;
+                model.AddManagerID = GetManager(db).ID;
+                db.Menus.Add(model);
+            }
+            else
+            {
+                db.Menus.Attach(model);
+                EntityEntry<Menus> entry = db.Entry(model);
+                entry.State = EntityState.Modified;
+                entry.Property(gp => gp.AddTime).IsModified = false;
+                entry.Property(gp => gp.AddManagerID).IsModified = false;
+            }
+            db.SaveChanges();
+            result.Data = model.ID;
+            result.Message = (ID == 0 ? "添加" : "修改") + "成功！";
+            return result;
+        }
+
+        [HttpPost]
+        public Result DeleteMenus(int[] IDs)
+        {
+            var result = new Result();
+            if (IDs == null)
+            {
+                result.Code = ResultCode.BadRequest;
+                result.Message = "删除失败, IDs为空!";
+                return result;
+            }
+
+            db.Menus.RemoveRange(db.Menus.Where(a => IDs.Contains(a.ID)).ToList());
+            db.SaveChanges();
+
+            result.Code = ResultCode.OK;
+            result.Message = "删除成功！IDs:" + ConvertTool.ToString(IDs, ",");
+            return result;
+        }
     }
 }
